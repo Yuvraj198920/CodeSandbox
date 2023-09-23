3e341f38af6a76ab95602fea5f13127e7c526d6c
yuvraj <yuvvraaj0flying@gmail.com>
2023-09-23 06:09:19 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { useState } from "react";
+
 export default function App() {
+  const [data, setData] = useState(null);
+
   async function getAdvice() {
     const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
     console.log(data);
+    setData(data);
   }
   return (
     <div>
       <h1>Hello World</h1>
+      {/* <p>{data}</p> */}
       <button onClick={getAdvice}>Get Advice</button>
     </div>
   );
