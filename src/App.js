a7dd7a5d97aecd51995d3faf5b3ab623682482de
yuvraj <yuvvraaj0flying@gmail.com>
2023-09-23 06:10:50 +0000

    Fetch and display data on dom

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { useState } from "react";
 
 export default function App() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState("");
 
   async function getAdvice() {
     const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
     console.log(data);
-    setData(data);
+    setData(data.slip.advice);
   }
   return (
     <div>
       <h1>Hello World</h1>
-      {/* <p>{data}</p> */}
+      <p>{data}</p>
       <button onClick={getAdvice}>Get Advice</button>
     </div>
   );
